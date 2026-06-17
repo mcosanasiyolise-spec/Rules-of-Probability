171798ec3dba02b4572a28972c3904a9c0633cad
mcosanasiyolise-spec <mcosanasiyolise@gmail.com>
2026-06-17 18:43:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Create the two sets of two guest lists
+set1 = {'A', 'B', 'C', 'D', 'E'}
+set2 = {'B', 'D', 'V', 'X', 'Y', 'Z'}
+
+# Find union of tw sets
+union = set1.union(set2)
+
+# Converting set into list
+# to find total guests to be invited in party
+total_guests = list(union)
+
+print("Total guests t be invited in party are :", len(total_guests))
+print("Guest List :", total_guests)
